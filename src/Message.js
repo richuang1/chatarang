35976311b86003e848b7e0d4d6513d75aa512868
richuang1 <richuang@iu.edu>
2018-07-03 15:23:41 -0400

    Add Message Form Component

--- src/Message.js
+++ src/Message.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import MessageList from './MessageList';
+
 
 const Message= (props)=>{
     return(
--- src/MessageList.js
+++ src/MessageList.js
@@ -1,37 +1,39 @@
 import React from 'react'
-import Message from './Message';
-const MessageList =() => {
 
-  const messages= [
-      {
-        id:1,
-        user: {
-            uid: "sdfsfeef3432",
-            displayName: "Rich",
-            email:"richuang@iu.edu",
+import Message from './Message'
 
-        },
-        body: "Chatting up a storm"
+const MessageList = () => {
+  const messages = [
+    {
+      id: 1,
+      user: {
+        uid: 'sdfs34849327',
+        displayName: 'Davey',
+        email: 'davey@fretless.com',
       },
-      {
-          id: 2
-          user: {
-              uid: "sdasdwdasd",
-              displayName: "Dana",
-              email. "dana@fretless.com",
-          },
-          body: 'This guy is so annoying. I hate my job.',
+      body: 'Chatting up a storm, yo!',
+    },
+
+    {
+      id: 2,
+      user: {
+        uid: 'sdlfkj35948',
+        displayName: 'Dana',
+        email: 'dana@fretless.com',
       },
+      body: 'This guy is so annoying. I hate my job.',
+    },
   ]
-    return(
-        
-        <div className="MessageList">
-         {messages.map(msg =>(
-             <Message message={msg} key ={msg.id} />
-         ))
-        }
-      </div>
-    )
+
+  return (
+    <div className="MessageList">
+      {
+        messages.map(msg => (
+          <Message message={msg} key={msg.id} />
+        ))
+      }
+    </div>
+  )
 }
 
 export default MessageList
