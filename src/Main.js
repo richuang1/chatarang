cadd33285cdc0897aab060a1660bc9eec02c1a8a
richuang1 <richuang@iu.edu>
2018-07-03 14:16:53 -0400

    classwork day 6

--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react'
 
-import Sidebar from './Sidebar'
-import Chat from './Chat'
-
 class Main extends Component {
   render() {
     return (
       <div className="Main">
-        <Sidebar />
-        <Chat />
+        Main
       </div>
     )
   }
