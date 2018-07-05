9fcb38e0946e3d0b4720eb4cb7916aa3dbf6693f
richuang1 <richuang@iu.edu>
2018-07-05 14:43:11 -0400

    Message Components Css Class

--- src/Message.js
+++ src/Message.js
@@ -1,22 +1,20 @@
 import React from 'react'
 
-const Message = (props) => {
+import Avatar from './Avatar'
+import Metadata from './Metadata'
+
+const Message = ({ message }) => {
   return (
-    <div className="Message" style={styles.message}>
-      {props.message.user.displayName}: {props.message.body}
+    <div className="Message">
+      <Avatar user={message.user} />
+      <div className="details">
+        <Metadata message={message} />
+        <div className="body">
+          {message.body}
+        </div>
+      </div>
     </div>
   )
-}
-const styles={
-  message: {
-    display: 'flex',
-    marginTop:'1rem',
-    padding:'0 1rem',
-  },
-  details:{
-    flex:1,
-    paddingLeft:'0.5rem',
-  }
 }
 
 export default Message
