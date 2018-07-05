e8c172fd0d1c668bd6f78f5b24e333e2ba586f74
richuang1 <richuang@iu.edu>
2018-07-05 14:44:01 -0400

    Style Message

--- src/Message.js
+++ src/Message.js
@@ -5,9 +5,12 @@ import Metadata from './Metadata'
 
 const Message = ({ message }) => {
   return (
-    <div className="Message">
+    <div
+      className="Message"
+      style={styles.message}
+    >
       <Avatar user={message.user} />
-      <div className="details">
+      <div style={styles.details}>
         <Metadata message={message} />
         <div className="body">
           {message.body}
@@ -15,6 +18,19 @@ const Message = ({ message }) => {
       </div>
     </div>
   )
+}
+
+const styles = {
+  message: {
+    display: 'flex',
+    marginTop: '1rem',
+    padding: '0 1rem',
+  },
+
+  details: {
+    flex: 1,
+    paddingLeft: '0.5rem',
+  },
 }
 
 export default Message
