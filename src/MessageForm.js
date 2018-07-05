b8965673918e2248bf524d1fc564a145727c84aa
richuang1 <richuang@iu.edu>
2018-07-05 13:57:55 -0400

    Add Avatar Component

--- src/MessageForm.js
+++ src/MessageForm.js
@@ -18,7 +18,7 @@ class MessageForm extends Component {
   render() {
     return (
       <form
-        className="MessageForm" style={style.messageForm}
+        className="MessageForm"
         onSubmit={this.handleSubmit}
       >
         <input
@@ -37,46 +37,5 @@ class MessageForm extends Component {
     )
   }
 }
-const styles= {
-  messageForm: {
-      backgroundColor: 'white',
-      height: '3rem',
-      display: 'flex',
-      alignItems: 'stretch',
-      border: '2px solid #999',
-      borderRadius: '0.5rem',
-      margin: '0.25rem',
-      padding: 0,
-    }
-    
-    chatIcon: {
-      display: 'flex'
-      borderRadius: '0.5rem',
-      alignItems: 'center',
-      backgroundColor: 'white',
-      color: '#ccc',
-      padding: 0 '0.5rem',
-      fontSize: '1.2rem',
-    },
-    
-    input: {
-      flex: 1;
-      fontSize: '1.2rem',
-      border: 0,
-    },
-    input:focus: {
-      outline: 0,
-    },
-      button: {
-      fontSize: '1.5rem',
-      backgroundColor: '#1A8FE3',
-      color: 'white',
-      paddingLeft: '1rem',
-      paddingRight: '1rem',
-      borderTopRightRadius: '0.5rem',
-      borderBottomRightRadius: '0.5rem',
-      border: '1px solid white',
-    }
-  }
-}
+
 export default MessageForm
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,23 +1,14 @@
 import React from 'react'
 
+import UserInfo from './UserInfo'
+
 const Sidebar = (props) => {
   return (
     <aside
       className="Sidebar"
       style={styles.sidebar}
     >
-      <div
-        className="UserInfo"
-        style={styles.children}
-      >
-        <div className="Avatar"></div>
-        <div className="user">
-          {props.user.displayName}
-        </div>
-        <a href="#">
-          <i className="fas fa-sign-out-alt"></i>
-        </a>
-      </div>
+      <UserInfo user={props.user} />
       <h1 style={styles.h1}>XTBC 18</h1>
       <nav
         className="RoomList"
@@ -32,6 +23,7 @@ const Sidebar = (props) => {
     </aside>
   )
 }
+
 const styles = {
   sidebar: {
     backgroundColor: '#333344',
