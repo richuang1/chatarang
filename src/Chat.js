aec08ad43c2a688ef04d6a6da6bfc912f0db77c4
richuang1 <richuang@iu.edu>
2018-07-09 14:19:52 -0400

    Firebase

--- src/Chat.js
+++ src/Chat.js
@@ -4,35 +4,30 @@ import ChatHeader from './ChatHeader'
 import MessageList from './MessageList'
 import MessageForm from './MessageForm'
 
-
+import base from './base'
 
 class Chat extends Component {
   constructor() {
     super()
 
     this.state = {
-      messages: [
-        {
-          id: 1,
-          user: {
-            uid: 'sdfs34849327',
-            displayName: 'Davey',
-            email: 'davey@fretless.com',
-          },
-          body: 'Chatting up a storm, yo!',
-        },
-
-        {
-          id: 2,
-          user: {
-            uid: 'sdlfkj35948',
-            displayName: 'Dana',
-            email: 'dana@fretless.com',
-          },
-          body: 'This guy is so annoying. I hate my job.',
-        },
-      ],
+      messages: [],
     }
+  }
+
+  componentDidMount() {
+    this.messagesRef = base.syncState(
+      'messages/general',
+      {
+        context: this,
+        state: 'messages',
+        asArray: true,
+      }
+    )
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.messagesRef)
   }
 
   addMessage = (body) => {
--- src/base.example.js
+++ src/base.example.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase/app'
+import 'firebase/database'
+import Rebase from 're-base'
 
 // Initialize Firebase
 const config = {
@@ -11,3 +13,8 @@ const config = {
 }
 
 const app = firebase.initializeApp(config)
+
+// Configure database and Rebase
+const db = firebase.database(app)
+const base = Rebase.createClass(db)
+export default base
