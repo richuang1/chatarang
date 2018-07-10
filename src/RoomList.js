b63c26619ec2ba8c8672c29887648010f21cb760
richuang1 <richuang@iu.edu>
2018-07-10 14:44:15 -0400

    Add new room button on the sidebar

--- src/RoomList.js
+++ src/RoomList.js
@@ -3,14 +3,22 @@ import { StyleSheet, css } from 'aphrodite'
 
 import Room from './Room'
 
-const RoomList = ({ rooms, setCurrentRoom }) => {
+const RoomList = ({ rooms, setCurrentRoom, showRoomForm }) => {
   return (
     <nav
       className={`RoomList ${css(styles.roomList)}`}
     >
-      <h2 className={css(styles.h2)}>
-        Rooms
-      </h2>
+      <div className={css(styles.heading)}>
+        <h2 className={css(styles.h2)}>
+          Rooms
+        </h2>
+        <button
+          className={css(styles.button)}
+          onClick={showRoomForm}
+        >
+          <i className="fas fa-plus-circle" title="Add a room"></i>
+        </button>
+      </div>
       <ul className={css(styles.list)}>
         {
           Object.keys(rooms).map(
@@ -42,6 +50,28 @@ const styles = StyleSheet.create({
     marginLeft: 0,
     paddingLeft: 0,
   },
+
+  heading: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
+  button: {
+    border: 0,
+    backgroundColor: 'transparent',
+    outline: 0,
+    padding: 0,
+    color: 'rgba(255,255,255,0.4)',
+    fontSize: '1rem',
+    cursor: 'pointer',
+    transition: 'color 0.25s ease-out',
+
+    ':hover': {
+      color: 'white',
+    },
+  },
+
 })
 
 export default RoomList
