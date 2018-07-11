fae509448bb0bbba0c97d9f959f8d2e7b78384a6
richuang1 <richuang@iu.edu>
2018-07-11 13:59:55 -0400

    Add react router

--- src/MessageList.js
+++ src/MessageList.js
@@ -3,6 +3,17 @@ import React, { Component } from 'react'
 import Message from './Message'
 
 class MessageList extends Component {
+  componentDidUpdate(prevProps) {
+    if (prevProps.messages.length < this.props.messages.length) {
+      this.scrollToBottom()
+    }
+  }
+
+  scrollToBottom = () => {
+    // "smooth" and other options not supported in IE
+    this.messagesEnd.scrollIntoView({ behavior: 'smooth' })
+  }
+
   render() {
     const { messages, room } = this.props
     return (
@@ -22,6 +33,7 @@ class MessageList extends Component {
             <Message message={msg} key={msg.id} />
           ))
         }
+        <div ref={el => this.messagesEnd = el}></div>
       </div>
     )
   }
